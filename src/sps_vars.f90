ffbb58d230b94117295fced3a2c90ba9d19bfbaa
mjpark <minjung.park@cfa.harvard.edu>
2024-07-21 16:54:34 -0400

    change logg, logt grids back to miles'

--- src/sps_vars.f90
+++ src/sps_vars.f90
@@ -381,15 +381,7 @@ MODULE SPS_VARS
   !max number of lines in tabulated SFH, LSF
   INTEGER, PARAMETER :: ntabmax=20000
   !dimensions of BaSeL library
-  #if (MILES)
-    INTEGER, PARAMETER :: ndim_logt=68, ndim_logg=19
-  #elif (C3K_LR)
-    INTEGER, PARAMETER :: ndim_logt=80, ndim_logg=14
-  #elif (C3K_HR)
-    INTEGER, PARAMETER :: ndim_logt=80, ndim_logg=14
-  #elif (C3K_NS)
-    INTEGER, PARAMETER :: ndim_logt=80, ndim_logg=14
-  #endif
+  INTEGER, PARAMETER :: ndim_logt=68, ndim_logg=19
   !number of O-rich, C-rich AGB spectra (and Aringer C-rich spec)
   INTEGER, PARAMETER :: n_agb_o=9, n_agb_c=5, n_agb_car=9
   !number of post-AGB spectra

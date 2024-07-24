2ae2b958d76b589a4223da5284fac28d80b5f3c1
mjpark <minjung.park@cfa.harvard.edu>
2024-07-24 15:43:21 -0400

    if miles use basel ndim_logt=68, ndim_logg=19.

--- src/sps_vars.f90
+++ src/sps_vars.f90
@@ -381,7 +381,11 @@ MODULE SPS_VARS
   !max number of lines in tabulated SFH, LSF
   INTEGER, PARAMETER :: ntabmax=20000
   !dimensions of BaSeL library
-  INTEGER, PARAMETER :: ndim_logt=68, ndim_logg=19
+  #if (MILES)
+    INTEGER, PARAMETER :: ndim_logt=68, ndim_logg=19
+  #else
+    INTEGER, PARAMETER :: ndim_logt=80, ndim_logg=14
+  #endif
   !number of O-rich, C-rich AGB spectra (and Aringer C-rich spec)
   INTEGER, PARAMETER :: n_agb_o=9, n_agb_c=5, n_agb_car=9
   !number of post-AGB spectra
